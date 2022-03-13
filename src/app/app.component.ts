dd7f85f69b76b6777a9216a7f4862455ee240a7d
smoonmare <smoonmare@gmail.com>
2022-03-12 20:39:39 -0800

    Added Services for optimizing the app. Cleaned up.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { OnInit, Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'services-assignment';
+
+  constructor() {}
+
+  ngOnInit() {}
+
 }
